b9a31b6bc896a754db21c1941d1ec5b50ad8cc20
svenk <svenkata1995@gmail.com>
2023-06-28 18:17:33 +0530

    cart script

--- common.sh
+++ common.sh
@@ -1,4 +1,4 @@
-
+app_user = roboshop
 print_head() {
   echo -e "\e[35m>>>$1>>>\e[0m"
 }
@@ -7,4 +7,7 @@ func_nodejs() {
   print_head "Setup NodeJS repos"
   curl -sL https://rpm.nodesource.com/setup_lts.x | bash
   yum install nodejs -y
+
+  print_head "Create application user"
+  useradd ${app_user}
 }
