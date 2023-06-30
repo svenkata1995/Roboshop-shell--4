37ba2ec7375701926c90059e196b6e21e0f59bf7
svenk <svenkata1995@gmail.com>
2023-06-30 12:47:33 +0530

    if script

--- if.sh
+++ if.sh
@@ -1,9 +1,12 @@
 fruit_name=mango
 quantity=2
 
-if[ "fruit_name"=="mango"]
+if[ "fruit_name" == "mango"]
 then
-  echo mango quantity -$quantity
+  echo mango quantity - $quantity
   else
     echo fruit does not exist
     fi
+
+    if [-z "$app_user" ]; then
+      echo input roboshop app user is missing
