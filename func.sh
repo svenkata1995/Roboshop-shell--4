2d613fdedb2c279e8a19ee4724ff00d4066f9314
svenk <svenkata1995@gmail.com>
2023-07-02 15:59:19 +0530

    func script

--- func.sh
+++ func.sh
@@ -1,4 +1,14 @@
-func_name() {
-    echo first argument =
-    echo secobd argument =
-    }
+date = 2/7/23
+echo today date is ${date}
+
+date = (date+%f)
+echo today date is $(date)
+
+Arth = ((2+5*8))
+echo Arth = ${Arth}
+
+echo scriptname - $0
+echo first argument - $1
+echo second argument - $2
+echo all arguments - $* 0r $@
+echo no. 0f arguments - $#
