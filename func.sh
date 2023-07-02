bdcb97a686967f932933b40682f8d506d07582d4
svenk <svenkata1995@gmail.com>
2023-07-02 16:31:12 +0530

    func script

--- func.sh
+++ func.sh
@@ -1,14 +1,16 @@
-date = 2/7/23
-echo today date is ${date}
+#date = 2/7/23
+#echo today date is ${date}
 
-date=$(date+%f)
-echo today date is $(date)
+#date=$(date+%f)
+#echo today date is $(date)
 
-Arth=$((2+5*8))
-echo Arth = ${Arth}
+#Arth=$
+#((2+5*8))
+#echo Arth = ${Arth}
 
-echo scriptname - $0
-echo first argument - $1
-echo second argument - $2
-echo all arguments - $* 0r $@
-echo no. 0f arguments - $#
+#echo scriptname - $0
+#echo first argument - $1
+#echo second argument - $2
+#echo all arguments - $* 0r $@
+#echo no. 0f arguments - $#
+echo -e "line1\nline2\line3"
